4f5e8a4d141fd4962819e3fcb262200b48c9fc26
Andrés Lucas Enciso <andresin87@gmail.com>
2017-01-16 00:12:06 +0100

    ADD uuid lib and cells

--- src/pages/sudoku/svg/SVG.js
+++ src/pages/sudoku/svg/SVG.js
@@ -35,11 +35,55 @@ export default class SudokuBoard extends Component {
   render() {
     let styles = this.getStyles();
 
+    let RowGridLine = (i, n) => <line data-n={n} x1={0} y1={(this.props.height*i)/n} x2={this.props.width} y2={(this.props.height*i)/n} style={{strokeWidth:1, stroke:'red'}}/>;
+    var Rows = [];
+    let RowGridLines = (n = 9) => {
+      for(let i = 0; i < n; i++) {
+        Rows.push(RowGridLine(i, n));
+      }
+    };
+    RowGridLines(9);
+
+    let ColGridLine = (i, n) => <line data-n={n} y1={0} x1={(this.props.width*i)/n} y2={this.props.height} x2={(this.props.width*i)/n} style={{strokeWidth:1, stroke:'red'}}/>;
+    var Cols = [];
+    let ColGridLines = (n = 9) => {
+      for(let i = 0; i < n; i++) {
+        Cols.push(ColGridLine(i, n));
+      }
+    };
+    ColGridLines(9);
+
+    let SqrGridLine = (i, n) => (
+      <rect
+        x={(Math.trunc(i%Math.sqrt(n))) * this.props.width / Math.sqrt(n)}
+        y={(Math.trunc(i/Math.sqrt(n))) * this.props.height / Math.sqrt(n)}
+        width={this.props.width/Math.sqrt(n)}
+        height={this.props.height/Math.sqrt(n)}
+        style={{
+          fill: 'none',
+          stroke: 'red',
+          strokeWidth: 2
+        }}
+        data-n={i}
+      />
+    );
+    var Sqrs = [];
+    let SqrGridLines = (n = 9) => {
+      for(let i = 0; i < n; i++) {
+        console.log(i, n);
+        Sqrs.push(SqrGridLine(i, n));
+      }
+    };
+    SqrGridLines(9);
+
     return (
       <svg style={styles.root}>
         <rect
           style={styles.rect}
         />
+        <g id="rows">{Rows}</g>
+        <g id="cols">{Cols}</g>
+        <g id="sqrs">{Sqrs}</g>
       </svg>
     );
   }
